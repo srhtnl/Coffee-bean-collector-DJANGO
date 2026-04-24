ad6822069fb3255f7302d98f8acd52357b9ad1c6
srhtnl <serhatpp97@gmail.com>
2026-04-24 16:28:36 +0200

    Admin.py geupdate zodat we de tastings en profielen inzien in de DB sectie

--- CoffeeTasting/base/admin.py
+++ CoffeeTasting/base/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Bean, Profile, Tasting
 # Register your models here.
+admin.site.register(Bean)
+admin.site.register(Profile)
+admin.site.register(Tasting)
